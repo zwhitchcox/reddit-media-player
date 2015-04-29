7c813a734978debd01bd35dfc9a8fb421630f3c5
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-29 07:07:51 -0400

    finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute','ngSanitize']);
+var app = angular.module('app', ['ngRoute','ngSanitize','ngTouch']);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
